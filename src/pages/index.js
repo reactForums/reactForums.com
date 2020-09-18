22ec3035bd28af2b2d33f20f302d3bba16241bf1
Moses Cosme <mosesmaxcosme@gmail.com>
2020-09-18 14:01:17 -0500

    homepage completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,32 @@ import SEO from "../components/seo"
 
 const IndexPage = ({ data }) => {
 	const features = data.wpgraphql.page.homeContent.features;
-  const { featureCtaText, featureCtaLink } = data.wpgraphql.page.homeContent;
+  const { 
+    featureCtaText, 
+    featureCtaLink,
+    heroHeader,
+    heroText,
+    header,
+    icon,
+    text, } = data.wpgraphql.page.homeContent;
 	return (
 		<Layout>
 			<SEO
 			title={data.wpgraphql.page.title}
 			description={data.wpgraphql.page.content}
 			/>
+      <div id="intro">
+        <div class="container">
+          <section>
+            <header>
+              <h2>{heroHeader}</h2>
+            </header>
+            <div class="content">
+              <p>{heroText}</p>
+            </div>
+          </section>
+        </div>
+      </div>
 			<div id="main">
 				<div className="container">
 					<ul className="featured">
@@ -30,6 +49,15 @@ const IndexPage = ({ data }) => {
 					</ul>
 				</div>
 			</div>
+        <div id="tweets">
+          <div class="container">
+            <span class="pennant">
+              <span class={`icon brands fa-${icon}`}></span>
+            </span>
+            <span class="tweet">{header}</span>
+            <p dangerouslySetInnerHTML={{__html: text}} />
+          </div>
+        </div>
 		</Layout>)
 }
 export default IndexPage
