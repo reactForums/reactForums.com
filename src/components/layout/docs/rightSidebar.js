f1529a0889f7eb9da53d7f16cf70a5273b5f9871
Moses Cosme <mosesmaxcosme@gmail.com>
2020-09-20 00:17:46 -0500

    dev doc sidebars

--- src/components/layout/docs/rightSidebar.js
+++ src/components/layout/docs/rightSidebar.js
@@ -2,38 +2,25 @@ import React from 'react';
 
 const RightSidebar = () => {
     return (
-        <div id="sidebar2" class="col-3 col-12-narrower">
-              <section>
-                <header>
-                  <h2>News & Announcements</h2>
-                </header>
-                <p>News and announcements regarding the software, the website, or the community.</p>
-                <ul class="alt">
-                  <li><a href="#">Moses to Run for U.S. Presidency</a></li>
-                  <li><a href="#">The Roadmap for 2021</a></li>
-                  <li><a href="#">Security Patch in New Version</a></li>
-                  <li><a href="#">The Beta Release</a></li>
-                  <li><a href="#">The New Community Board</a></li>
-                  <li><a href="#">Humble Beginnings</a></li>
-                </ul>
-              </section>
-              <section>
-                <header>
-                  <h2>Archive</h2>
-                </header>
-                <ul class="alt">
-                  <li><a href="#">May 2020</a></li>
-                  <li><a href="#">June 2020</a></li>
-                  <li><a href="#">July 2020</a></li>
-                  <li><a href="#">August 2020</a></li>
-                  <li><a href="#">September 2020</a></li>
-                  <li><a href="#">October 2020</a></li>
-                  <li><a href="#">November 2020</a></li>
-                  <li><a href="#">December 2020</a></li>
-                  <li><a href="#">January 2021</a></li>
-                </ul>
-              </section>
-            </div>
+    <div id="sidebar2" class="col-3 col-12-narrower">
+      <section>
+        <header>
+          <h2>Developer Docs</h2>
+        </header>
+        <ul class="alt">
+          <li><a href="/">01. About</a></li>
+          <li><a href="/">02. Contribution</a></li>
+          <li><a href="/">03. Features</a></li>
+          <li><a href="/">04. User Auth</a></li>
+          <li><a href="/">05. Routin</a></li>
+          <li><a href="/">06. Rendering</a></li>
+          <li><a href="/">07. API</a></li>
+          <li><a href="/">08. Database</a></li>
+          <li><a href="/">09. Globals</a></li>
+          <li><a href="/">10. Customization</a></li>
+        </ul>
+      </section>
+    </div>
 
     )
 }
