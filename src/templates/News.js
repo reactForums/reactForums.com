965e26ba3f0008c8b8ac219209b508eeb4305753
Moses Cosme <mosesmaxcosme@gmail.com>
2020-09-22 14:24:36 -0500

    styled blog posts

--- src/templates/News.js
+++ src/templates/News.js
@@ -2,21 +2,36 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
+import NewsSidebar from "../components/layout/news/sidebar.js"
+import moment from 'moment/moment'
 
 const NewsTemplate = ({ data }) => {
-  const { title, excerpt, date, content } = data.wpgraphql.post;
+  const { title, excerpt, date, content, author } = data.wpgraphql.post;
+  const posts = data.wpgraphql.posts.nodes;
   return (
   <Layout>
     <SEO title={title} description={excerpt} />
-    <h1>{title}</h1>
-    <p>
-      Written on {date}
-    </p>
-    <div dangerouslySetInnerHTML={{ __html: content }} />
+    <div id="main">
+      <div class="container">
+        <div class="row">
+          <NewsSidebar posts={posts} />
+          <div id="content" class="col-8">
+            <article>
+              <header>
+                <h2>{title}</h2>
+                <span class="byline">Written by {author.node.name} on {moment(date).format('MMMM D, YYYY')}.</span>
+              </header>
+              <div dangerouslySetInnerHTML={{__html: content}} />
+            </article>
+          </div>
+        </div>
+      </div>
+    </div>
   </Layout>
   )
 }
 export default NewsTemplate
+
 export const query = graphql`
   query($id: ID!) {
     wpgraphql {
@@ -25,6 +40,18 @@ export const query = graphql`
         content
         excerpt
         date
+        author {
+          node {
+            name
+          }
+        }
+      }
+      posts {
+        nodes {
+          title
+          slug
+          date
+        }
       }
     }
   }
