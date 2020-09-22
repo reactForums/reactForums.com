05db436602054fed780f3257ab2160280c85f3ff
Moses Cosme <mosesmaxcosme@gmail.com>
2020-09-22 11:08:54 -0500

    archive widget dynamic and functional

--- src/components/layout/news/sidebar.js
+++ src/components/layout/news/sidebar.js
@@ -1,39 +1,47 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import moment from 'moment/moment'
 
 const NewsSidebar = (props) => {
+  const dates = [];
+  const archive = [];
+
+  props.posts.map(post => {
+    return dates.push(moment(post.date).format('MMMM YYYY'))
+  })
+  const uniqueDates = new Set(dates);
+
+  uniqueDates.forEach(date => {
+    return archive.push(date);
+  })
+
     return (
         <div id="sidebar" class="col-4 col-12-narrower">
-              <section>
-                <header>
-                  <h2>News & Announcements</h2>
-                </header>
-                <ul class="alt">
-                  {props.posts.map(post => {
-                    return (
-                      <li><Link to={`/news/${post.slug}`}>{post.title}</Link></li>
-                    )
-                  })}
-                </ul>
-              </section>
-              <section>
-                <header>
-                  <h2>Archive</h2>
-                </header>
-                <ul class="alt">
-                  <li><a href="/">May 2020</a></li>
-                  <li><a href="/">June 2020</a></li>
-                  <li><a href="/">July 2020</a></li>
-                  <li><a href="/">August 2020</a></li>
-                  <li><a href="/">September 2020</a></li>
-                  <li><a href="/">October 2020</a></li>
-                  <li><a href="/">November 2020</a></li>
-                  <li><a href="/">December 2020</a></li>
-                  <li><a href="/">January 2021</a></li>
-                </ul>
-              </section>
-            </div>
-
+          <section>
+            <header>
+              <h2>News & Announcements</h2>
+            </header>
+            <ul class="alt">
+              {props.posts.map(post => {
+                return (
+                  <li><Link to={`/news/${post.slug}`}>{post.title}</Link></li>
+                )
+              })}
+            </ul>
+          </section>
+          <section>
+            <header>
+              <h2>Archive</h2>
+            </header>
+            <ul class="alt">
+              {archive.map(date => {
+                return (
+                  <li><Link to={`/news/archive/${date}`}>{date}</Link></li>
+                )
+              })}
+            </ul>
+          </section>
+        </div>
     )
 }
 
